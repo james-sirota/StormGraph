f58a47961ced3d27d6d37687cd8ae7cdbd2b3928
James Sirota <jsirota@apache.org>
2018-11-30 14:51:22 -0700

    added license headers

--- src/test/java/metron/graph/JanusDAOTest.java
+++ src/test/java/metron/graph/JanusDAOTest.java
@@ -1,8 +1,20 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package metron.graph;
 
-import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import org.apache.hadoop.hbase.shaded.org.junit.Before;
@@ -10,8 +22,6 @@ import org.apache.hadoop.hbase.shaded.org.junit.Before;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import metron.graph.EdgeTypes;
-import metron.graph.JanusDAO;
 
 public class JanusDAOTest extends TestCase {
 
