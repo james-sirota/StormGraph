2691271eb13090009bc8e91cb301840f76838044
James Sirota <jsirota@apache.org>
2018-12-03 13:43:06 -0700

    added proper logging to graph topology submitter

--- src/main/java/metron/graph/GraphTopology.java
+++ src/main/java/metron/graph/GraphTopology.java
@@ -34,66 +34,142 @@ import org.apache.storm.tuple.Values;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.esotericsoftware.minlog.Log;
+
 public class GraphTopology {
 
 	private static final Logger logger = LoggerFactory.getLogger(GraphTopology.class);
 
 	public static void main(String[] args) throws Exception {
 
-		Config conf = readConfigFromFile(args[0]);
+		Config conf = readConfigFromFile(args[0], logger);
 
 		TopologyBuilder builder = new TopologyBuilder();
-		
-		if(Boolean.parseBoolean(conf.get("top.generatorSpoutEnabled").toString()))
-		{
-		builder.setSpout(conf.get("top.spout.name").toString(), new TelemetryLoaderSpout(),
-				Integer.parseInt(conf.get("top.spout.parallelism").toString()));
-		}
-		else
-		{
+
+		String spoutName = conf.get("top.spout.name").toString();
+		logger.debug("Set spoutName " + " to " + spoutName);
+
+		int spoutParallelism = Integer.parseInt(conf.get("top.spout.parallelism").toString());
+		logger.debug("Set spoutParallelism" + " to " + spoutParallelism);
+
+		boolean generateData = Boolean.parseBoolean(conf.get("top.generatorSpoutEnabled").toString());
+		logger.debug("Set  generateData" + " to " + generateData);
+
+		if (generateData) {
+
+			logger.trace("Started initializing generator spout");
+			logger.debug(
+					"Setting up generator spout with name " + spoutName + " and parallelism of " + spoutParallelism);
+			builder.setSpout(spoutName, new TelemetryLoaderSpout(), spoutParallelism);
+			logger.trace("Finished initializing the generator spout");
+
+		} else {
+
+			logger.trace("Started initializing kafka spout");
+
 			String bootStrapServers = conf.get("top.spout.kafka.bootStrapServers").toString();
+			logger.debug("Set bootStrapServers" + " to " + bootStrapServers);
+
 			String topic = conf.get("top.spout.kafka.topic").toString();
+			logger.debug("Set topic" + " to " + topic);
+
 			String consumerGroupId = conf.get("top.spout.kafka.consumerGroupId").toString();
+			logger.debug("Set consumerGroupId" + " to " + consumerGroupId);
+
 			Long offsetCommitPeriodMs = Long.parseLong(conf.get("top.spout.kafka.consumerGroupId").toString());
-			int initialDelay=Integer.parseInt(conf.get("top.spout.kafka.retry.initialDelay").toString());
-			int delayPeriod=Integer.parseInt(conf.get("top.spout.kafka.retry.delayPeriod").toString());
-			int maxDelay=Integer.parseInt(conf.get("top.spout.kafka.retry.maxDelay").toString());
-			int maxUncommittedOffsets=Integer.parseInt(conf.get("top.spout.kafka.maxUncommittedOffsets").toString());
-			
-			KafkaSpoutRetryService kafkaSpoutRetryService =  new KafkaSpoutRetryExponentialBackoff(
+			logger.debug("Set offsetCommitPeriodMs" + " to " + offsetCommitPeriodMs);
+
+			int initialDelay = Integer.parseInt(conf.get("top.spout.kafka.retry.initialDelay").toString());
+			logger.debug("Set initialDelay" + " to " + initialDelay);
+
+			int delayPeriod = Integer.parseInt(conf.get("top.spout.kafka.retry.delayPeriod").toString());
+			logger.debug("Set delayPeriod" + " to " + delayPeriod);
+
+			int maxDelay = Integer.parseInt(conf.get("top.spout.kafka.retry.maxDelay").toString());
+			logger.debug("Set maxDelay" + " to " + maxDelay);
+
+			int maxUncommittedOffsets = Integer.parseInt(conf.get("top.spout.kafka.maxUncommittedOffsets").toString());
+			logger.debug("Set maxUncommittedOffsets" + " to " + maxUncommittedOffsets);
+
+			logger.trace("Started initializing kafkaSpoutRetryService");
+			logger.debug("Initializing kafkaSpoutRetryService " + " with initial delay " + initialDelay
+					+ " delay period " + delayPeriod + " max delay " + maxDelay);
+
+			KafkaSpoutRetryService kafkaSpoutRetryService = new KafkaSpoutRetryExponentialBackoff(
 					KafkaSpoutRetryExponentialBackoff.TimeInterval.microSeconds(initialDelay),
-			        KafkaSpoutRetryExponentialBackoff.TimeInterval.milliSeconds(delayPeriod), 
-			        Integer.MAX_VALUE, 
-			        KafkaSpoutRetryExponentialBackoff.TimeInterval.seconds(maxDelay));
-			
-			KafkaSpoutConfig<String, String> spoutConf =  KafkaSpoutConfig.builder(bootStrapServers, topic)
-			        .setProp(ConsumerConfig.GROUP_ID_CONFIG,consumerGroupId)
-			        .setOffsetCommitPeriodMs(offsetCommitPeriodMs)
-			        .setFirstPollOffsetStrategy(KafkaSpoutConfig.FirstPollOffsetStrategy.UNCOMMITTED_LATEST)
-			        .setMaxUncommittedOffsets(maxUncommittedOffsets)
-			        .setRetry(kafkaSpoutRetryService)
-			        .setRecordTranslator((r) -> new Values(r.topic(), r.partition(), r.offset(), r.key(), r.value()),
-			                new Fields("topic", "partition", "offset", "key", "value"))
-			        .build();
-			
-			builder.setSpout(conf.get("top.spout.name").toString(), new KafkaSpout<String, String>(spoutConf),
-					Integer.parseInt(conf.get("top.spout.parallelism").toString()));
+					KafkaSpoutRetryExponentialBackoff.TimeInterval.milliSeconds(delayPeriod), Integer.MAX_VALUE,
+					KafkaSpoutRetryExponentialBackoff.TimeInterval.seconds(maxDelay));
+
+			logger.trace("Finished initializing kafkaSpoutRetryService");
+
+			String tupleFieldTopic = conf.get("top.spout.kafka.tupleFieldTopic").toString();
+			logger.debug("Set tupleFieldTopic" + " to " + tupleFieldTopic);
+
+			String tupleFieldPartition = conf.get("top.spout.kafka.tupleFieldPartition").toString();
+			logger.debug("Set tupleFieldPartition" + " to " + tupleFieldPartition);
+
+			String tupleFieldOffset = conf.get("top.spout.kafka.tupleFieldOffset").toString();
+			logger.debug("Set tupleFieldOffset" + " to " + tupleFieldOffset);
+
+			String tupleFieldKey = conf.get("top.spout.kafka.tupleFieldKey").toString();
+			logger.debug("Set tupleFieldKey" + " to " + tupleFieldKey);
+
+			String tupleFieldValue = conf.get("top.spout.kafka.tupleFieldValue").toString();
+			logger.debug("Set tupleFieldValue" + " to " + tupleFieldValue);
+
+			logger.trace("Started initializing spoutConf");
+			KafkaSpoutConfig<String, String> spoutConf = KafkaSpoutConfig.builder(bootStrapServers, topic)
+					.setProp(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId)
+					.setOffsetCommitPeriodMs(offsetCommitPeriodMs)
+					.setFirstPollOffsetStrategy(KafkaSpoutConfig.FirstPollOffsetStrategy.UNCOMMITTED_LATEST)
+					.setMaxUncommittedOffsets(maxUncommittedOffsets).setRetry(kafkaSpoutRetryService)
+					.setRecordTranslator((r) -> new Values(r.topic(), r.partition(), r.offset(), r.key(), r.value()),
+							new Fields(tupleFieldTopic, tupleFieldPartition, tupleFieldOffset, tupleFieldKey,
+									tupleFieldValue))
+					.build();
+			logger.trace("Finished initializing spoutConf");
+
+			builder.setSpout(spoutName, new KafkaSpout<String, String>(spoutConf), spoutParallelism);
+
 		}
 
-		builder.setBolt(conf.get("top.mapperbolt.name").toString(), new MapperBolt(),
-				Integer.parseInt(conf.get("top.mapperbolt.parallelism").toString()))
-				.shuffleGrouping(conf.get("top.spout.name").toString());
+		String mapperBoltName = conf.get("top.mapperbolt.name").toString();
+		logger.debug("Set mapperBoltName" + " to " + mapperBoltName);
 
-		builder.setBolt(conf.get("top.graphbolt.name").toString(), new JanusBolt(),
-				Integer.parseInt(conf.get("top.graphbolt.parallelism").toString()))
+		int spoutBoltParallelism = Integer.parseInt(conf.get("top.mapperbolt.parallelism").toString());
+		logger.debug("Set spoutBoltParallelism" + " to " + spoutBoltParallelism);
+		
+		logger.debug("Initializing " + mapperBoltName + " with parallelism " + spoutBoltParallelism);
+		builder.setBolt(mapperBoltName, new MapperBolt(), spoutBoltParallelism).shuffleGrouping(spoutName);
+		
+		String graphBoltName = conf.get("top.graphbolt.name").toString();
+		logger.debug("Set graphBoltName" + " to " + graphBoltName);
+		
+		int graphBoltParallelism = Integer.parseInt(conf.get("top.graphbolt.parallelism").toString());
+		logger.debug("Set graphBoltParallelism" + " to " + graphBoltParallelism);
+		
+		
+		logger.debug("Initializing " + graphBoltName + " with parallelism " + graphBoltParallelism);
+		builder.setBolt(graphBoltName, new JanusBolt(),graphBoltParallelism)
 				.shuffleGrouping(conf.get("top.mapperbolt.name").toString());
 
-		conf.setDebug(Boolean.getBoolean("top.debug"));
+		boolean debugMode = Boolean.getBoolean("top.debug");
+		logger.debug("Set debugMode" + " to " + debugMode);
+		
+		logger.trace("Updating conf.setDebug with value " + debugMode);
+		conf.setDebug(debugMode);
+		
+		int numWorkers = Integer.parseInt(conf.get("top.numWorkers").toString());
+		logger.debug("Set numWorkers" + " to " + numWorkers);
 
-		conf.setNumWorkers(Integer.parseInt(conf.get("top.numWorkers").toString()));
+		logger.trace("Updating conf.setNumWorkers with value " + numWorkers);
+		conf.setNumWorkers(numWorkers);
 
 		boolean localDeploy = Boolean.parseBoolean(conf.get("top.localDeploy").toString());
+		logger.debug("Set localDeploy" + " to " + localDeploy);
+		
 		String topologyName = conf.get("top.name").toString();
+		logger.debug("Set topologyName" + " to " + topologyName);
 
 		if (localDeploy) {
 			LocalCluster cluster = new LocalCluster();
@@ -104,7 +180,7 @@ public class GraphTopology {
 
 	}
 
-	public static Config readConfigFromFile(String filename) throws IOException {
+	public static Config readConfigFromFile(String filename, Logger log) throws IOException {
 		Config conf = new Config();
 		System.out.println("[METRON] Reading config file: " + filename);
 
@@ -119,6 +195,7 @@ public class GraphTopology {
 			if (strLine.length() != 0 && !(strLine.charAt(0) == '#')) {
 				String[] parts = strLine.split("=");
 				conf.put(parts[0], parts[1]);
+				log.debug("Setting property " + parts[0] + " to " + parts[1]);
 			}
 		}
 
