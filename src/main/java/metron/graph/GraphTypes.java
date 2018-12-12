87b241be610c5d271669c186f72c0b0d786497eb
James Sirota <jsirota@apache.org>
2018-12-12 12:53:00 -0700

    added graph types

---
+++ src/main/java/metron/graph/GraphTypes.java
@@ -0,0 +1,53 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package metron.graph;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class GraphTypes implements Serializable{
+	/**
+	 * 
+	 */
+	private final long serialVersionUID = 5857601064882508913L;
+
+	
+	private ArrayList<String> edgeTypes = new ArrayList<String>();
+	private ArrayList<String> vertexTypes = new ArrayList<String>();
+	
+	public GraphTypes()
+	{
+		edgeTypes.add("connectsTo");
+		edgeTypes.add("uses");
+		edgeTypes.add("usedBy");
+		
+		vertexTypes.add("host");
+		vertexTypes.add("user");
+		vertexTypes.add("ipAddress");
+	}
+	
+	public ArrayList<String> getEdgeTypes()
+	{
+		return edgeTypes;
+	}
+	
+	public ArrayList<String> getVertexTypes()
+	{
+		return vertexTypes;
+	}
+	
+	
+}
